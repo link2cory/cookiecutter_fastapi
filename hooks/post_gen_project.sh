f91edfb076bd57e064282df6052cc0e3d5fd1e48
Cory Perkins <link2cory@gmail.com>
2020-02-20 22:30:10 -0800

    moved the bootstrapping scripts into the rendered project so they can be reused or referenced by the user

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,48 +1,13 @@
 #!/bin/sh
 
-# since black is in pre-release we need to do this
-pipenv lock --pre
+# go get the gitignore template from github
+# it is important that this file not exist until project render time because 
+# there are files in the template that are intended to stay on the local 
+# machine, but git will pickup gitignores from any directory and remove
+# them from the template entirely
+curl -o .gitignore https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore
 
-# run pipenv install in the project directory
-pipenv install --dev
-
-# generate a requirements file
-pipenv lock -r > requirements.txt
-
-# build the docker image(s)
-#{% if cookiecutter.docker_support == "yes" %}
-#docker build . -t {{cookiecutter.project_name}}
-#{% endif %}
-
-# run the initial tests
-#pytest
-
-# create a github respository
-curl -u {{cookiecutter.github_username}}:{{cookiecutter.github_password}} \
-	--data '@github_repo_config.json' \
-	https://api.github.com/user/repos
-
-# remove the github_repo_config file
-rm github_repo_config.json
-
-# create a local git repo
-git init
-git add -A
-git commit --message "initial commit"
-
-# push the local repo up to github
-git remote add origin \
-	git@github.com:{{cookiecutter.github_username}}/{{cookiecutter.project_name}}.git
-git push -u origin master
-
-# create a jenkins job for this project
-jenkins-jobs --conf jenkins/jenkins_jobs.ini update jenkins/job_config.yaml
-
-# setup a github webhook to initial builds on push
-curl -u {{ cookiecutter.github_username }}:{{ cookiecutter.github_password }} \
-	--data '@github_hook_build_config.json' \
-	https://api.github.com/repos/{{ cookiecutter.github_username }}/{{ cookiecutter.project_name }}/hooks
-
+./scripts/bootstrap
 
 # return the result of the last command
 exit $?
